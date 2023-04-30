eaa40f1baed4db1502968303e04e48edefb81cd4
Will Richardson <rich1051@umn.edu>
2023-04-30 13:37:51 -0500

    Added .push to add my albums to end of array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -7,4 +7,17 @@ function addToCollection(title, artist, yearPublished) {
         artist: artist,
         yearPublished: yearPublished
     };
+
+collection.push(album);
+
+return(album);
 }
+
+console.log(addToCollection('The Young and the Hopeless', 'Good Charlotte', 2002));
+console.log(addToCollection('American Idiot', 'Green Day', 2004));
+console.log(addToCollection('A Night at the Opera', 'Queen', 1975));
+console.log(addToCollection('21st Century Breakdown', 'Green Day', 2009));
+console.log(addToCollection('Prounounced Leh-Nerd Skin-Nerd', 'Lynyrd Skynyrd', 1973));
+console.log(addToCollection('Goodbye Yellow Brick Road', 'Elton John', 1973));
+
+console.log(collection);
